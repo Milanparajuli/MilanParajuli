231c1b05e0361bfbd5e32e7859e8a548384d0404
Milanparajuli <milanbhai2058@gmail.com>
2022-08-23 11:24:49 +0545

    make navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './clientside/home/home.component';
+import { FooterComponent } from './common/component/footer/footer.component';
+import { NavComponent } from './common/component/nav/nav.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:HomeComponent,
+  },
+  // {
+  //   path:'nav',
+  //   component:NavComponent,
+  // },
+  // {
+  //   path:'foot',
+  //   component:FooterComponent,
+  // }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { NavComponent } from './common/component/nav/nav.component';
 import { SidebarComponent } from './common/component/sidebar/sidebar.component';
 import { FooterComponent } from './common/component/footer/footer.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './clientside/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     SidebarComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
