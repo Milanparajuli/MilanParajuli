300ced1b8489a6f7ba7155ce90e5d940f79c18a1
Milanparajuli <milanbhai2058@gmail.com>
2022-08-23 10:50:27 +0545

    initialized commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './common/component/nav/nav.component';
+import { SidebarComponent } from './common/component/sidebar/sidebar.component';
+import { FooterComponent } from './common/component/footer/footer.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    SidebarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
