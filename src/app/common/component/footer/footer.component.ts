2f2b216a26cde303b54980b9e7db6b106982da52
MilanParajuli <milan.parajuli@sbsolutionsnepal.com>
2025-04-06 14:45:06 +0545

    fixed

--- src/app/common/component/footer/footer.component.ts
+++ src/app/common/component/footer/footer.component.ts
@@ -6,10 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
-
+currentDate: any;
   constructor() { }
 
   ngOnInit(): void {
+    this.currentDate = (new Date()).getFullYear();
   }
 
 }
